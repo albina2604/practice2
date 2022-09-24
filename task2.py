b7450bde289bedbf5cd23168628eeca114141b01
albina2604 <albinamilshyna2604@gmail.com>
2022-09-24 20:08:12 +0300

    Updated task.2

--- task2.py
+++ task2.py
@@ -27,11 +27,10 @@ def next_date(day, month, year):
         (day, month, year):следующая дата
     """
     day += 1
-    if day > (days_in_month[month]
-              + leap_year(year) if month == 2 else 0):
+    if day > (days_in_month[month] + leap_year(year) if month == 2 else 0):
         day = 1
         month += 1
-    elif month == 13:
+    if month == 13:
         month = 1
         year += 1
     return day, month, year
